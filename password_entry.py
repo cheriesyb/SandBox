6e0bf0f9d7ba546f549735fe50e419dbcdc32d87
jc500052 <cherie.barringhaus@my.jcu.edu.au>
2018-08-14 14:47:36 +1000

    "Add password check program"

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,18 @@
 """Cherie"""
+
+MIN_LENGTH = 4
+
+
+def main():
+    get_password = validate_password(MIN_LENGTH)
+    print('*' * len(get_password))
+
+
+def validate_password(min_length):
+    password = input("Please enter password: ")
+    if len(password) < MIN_LENGTH:
+        print("Password is too short.")
+        password = input("Please enter password: ")
+        return password
+
+main()

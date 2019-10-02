ed454c183e7ad5e1679595e3f04fa012cd6ec749
chezza281 <cbarring@our.ecu.edu.au>
2019-10-02 16:54:22 +0800

    Change string format

--- list_files.py
+++ list_files.py
@@ -1,6 +1,6 @@
 import os
 
-print("The files and folders in {} are:".format(os.getcwd()))
+print(f"The files and folders in {os.getcwd()} are: ")
 items = os.listdir('.')
 for item in items:
     print(item)
